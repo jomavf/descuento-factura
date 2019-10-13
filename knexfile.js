38682d7f77dab63d2cb2f666f53b6887e5eea8e9
ZetaGH <jose.veliz.francia@gmail.com>
2019-10-13 12:35:02 -0500

    :sparkles: Se agrego la variable de entorno de produccion con la cadena de conexion a base de datos

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,10 @@ module.exports = {
         client: 'pg',
         connection: 'postgres://localhost/descuento-factura'
     },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL
+    },
     test: {
         client: 'pg',
         connection: 'postgres://localhost/test-descuento-factura'
