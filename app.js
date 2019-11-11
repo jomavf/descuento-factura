db53352b3fe5977b873610011d7f17d17745ad2b
ZetaGH <jose.veliz.francia@gmail.com>
2019-11-11 11:34:37 -0500

    cors added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const app = express();
 
@@ -11,6 +12,7 @@ const facturas = require('./api/facturas');
 const contratos = require('./api/contratos');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
